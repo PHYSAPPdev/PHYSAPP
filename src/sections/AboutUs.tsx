89a8a2481e5f8ee871bb12682ce55cf090f8dc10
Luz Coelho <165476743+Luz-Coelho@users.noreply.github.com>
2025-05-13 09:42:56 -0300

    Update AboutUs.tsx

--- src/sections/AboutUs.tsx
+++ src/sections/AboutUs.tsx
@@ -1,12 +1,30 @@
 import { useTranslation } from 'react-i18next';
 import Title from '../components/Title';
+import '../styles/pages/aboutus.sass';
 
 const AboutUs = () => {
-    const { t } = useTranslation();    
+    const { t } = useTranslation();
 
     return (
         <>
             <Title title={t("AboutUs")} />
+            <div className="container">
+                <div className="ab-1">
+                    <div className="container aboutus">
+                        <div className="aboutus-img logo">
+                            <img src="/physapp.png" alt="aa" />
+                        </div>
+                        <p className="aboutus-text a">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum corporis aliquam cupiditate explicabo quisquam expedita enim dolor quibusdam, vero excepturi soluta quod aut dolorem aperiam quo error accusamus at! In?</p>
+                    </div>
+
+                    <div className="container aboutus">
+                        <p className="aboutus-text b">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum corporis aliquam cupiditate explicabo quisquam expedita enim dolor quibusdam, vero excepturi soluta quod aut dolorem aperiam quo error accusamus at! In?</p>
+                        <div className="aboutus-img banner">
+                            <img src="/banner.png" alt="aa" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
